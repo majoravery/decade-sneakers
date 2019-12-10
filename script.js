a56b5566762792e2e1b80e8a3a36f50584a93c02
Avery <spacemup@users.noreply.github.com>
2019-12-10 17:54:03 +0100

    scrolling animation

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-window.onload = function() {
-  var navBtns = document.querySelectorAll('.nav-button');
-  navBtns.forEach(function(btn) {
-    btn.addEventListener('click', navBtnClickHandler);
-  });
-
-  var navBtnClickHandler = function(e) {
-    console.log(e.target.id);
+(function() {
+  var addNavBtnClickHandlers = function() {
+    var navBtns = document.querySelectorAll('.nav-button');
+    navBtns.forEach(function(btn) {
+      btn.addEventListener('click', navBtnClickHandler);
+    });
   }
-}
+  
+  var navBtnClickHandler = function(e) {
+    console.log({ e, id: e.target.id });
+    e.preventDefault();
+    var year = e.target.innerHTML;
+    var sectionOffset = document.getElementById(year).getBoundingClientRect().top;
+
+    window.scrollTo({
+      top: parseInt(window.pageYOffset) + parseInt(sectionOffset),
+      behavior: 'smooth'
+    });
+
+    console.log(document.getElementById(year));
+    console.log({year, sectionOffset});
+  }
+
+  window.onload = function() {
+    addNavBtnClickHandlers();
+  }
+})();
